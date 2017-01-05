49b8fdc36a6f3c07d8d7a22d151f3c15aa92c9b1
nandhu92 <nandhini0716@gmail.com>
2017-01-04 21:31:04 -0800

    mongo change

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -2,7 +2,7 @@
 var mongoose = require('mongoose');
 var fs = require('fs');
 var path = require('path');
-mongoose.connect('mongodb://localhost/recipe');
+mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/recipe');
 var models_path =path.join( __dirname , '/../models');
 fs.readdirSync(models_path).forEach(function(file) {
   if(file.indexOf('.js') > 0) {
