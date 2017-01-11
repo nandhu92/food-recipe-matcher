b883a33cacf7deeb057d2aa8eb1c88e30d1e988d
nandhu92 <nandhini0716@gmail.com>
2017-01-10 22:55:50 -0800

    mongo change

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -2,7 +2,7 @@
 var mongoose = require('mongoose');
 var fs = require('fs');
 var path = require('path');
-mongoose.connect('mongodb://localhost/recipe');
+mongoose.connect('mongodb://admin:password@ds155428.mlab.com:55428/recipe');
 //process.env.MONGOLAB_URI
 //|| 'mongodb://localhost/recipe'
 //mongodb://admin:password@ds155428.mlab.com:55428/recipe
